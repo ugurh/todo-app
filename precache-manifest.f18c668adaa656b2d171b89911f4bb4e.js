62c01719ba193d23050aa4e2cb87b2ae5743dde9
hrnugr0000 <ugur.harun@yandex.com>
2020-01-31 15:58:23 +0300

    Updates

---
+++ precache-manifest.f18c668adaa656b2d171b89911f4bb4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac36ed7d28e81d01d253808b8f056dfc",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "5859d0a1203be58fcf59",
+    "url": "/todo-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "356a5e02e0e69e7c6a72",
+    "url": "/todo-app/static/js/2.6e78796b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-app/static/js/2.6e78796b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5859d0a1203be58fcf59",
+    "url": "/todo-app/static/js/main.b0f487a9.chunk.js"
+  },
+  {
+    "revision": "b66ecab6fc712c6d22a7",
+    "url": "/todo-app/static/js/runtime-main.e77e13dd.js"
+  }
+]);
